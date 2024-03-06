caec1fc50086c504168033b5ade1a64e1ff26f98
Dmitry <dimonchechulov@gmail.com>
2024-03-06 16:54:56 +0300

    Fixed methods and delete 'def' and 'class'

--- parser.py
+++ parser.py
@@ -11,11 +11,7 @@ def count_variables(node):
             while isinstance(n.value, ast.Attribute):
                 s = n.value.attr + "." +s
                 n = n.value
-
-            if isinstance(n.value, ast.Name):
-                s = n.value.id + "." +s
-            else:
-                s = n.value.func.attr + '().' +s
+            s = n.value.id + "." +s
             if s[:-1] in vars.keys():
                 vars[s[:-1] ] -= 1
             else:
@@ -233,12 +229,9 @@ def count_func(node,vars):
         "assert":0,
         "with":0,
         "class":0,
-        "def":0,
-        "[]":0,
+        "def":0
     }
     for n in ast.walk(node):
-        if isinstance(n, ast.ClassDef):
-            funcs["class"]+=1
         if isinstance(n, ast.With):
             funcs["with"]+=1
         if isinstance(n, ast.Assert):
@@ -255,13 +248,13 @@ def count_func(node,vars):
             funcs["yeild from"]+=1
         if isinstance(n, ast.Call):
             if isinstance(n.func, ast.Attribute):#methods
-                if n.func.attr  in vars:
-                    vars[n.func.attr] -= 1
+                if n.func.value.id+"."+n.func.attr  in vars:
+                    vars[n.func.value.id+"."+n.func.attr] -= 1
 
-                if n.func.attr not in funcs:
-                    funcs[n.func.attr]=1
+                if n.func.value.id+"."+n.func.attr not in funcs:
+                    funcs[n.func.value.id+"."+n.func.attr]=1
                 else:
-                    funcs[n.func.attr] += 1
+                    funcs[n.func.value.id+"."+n.func.attr] += 1
             elif isinstance(n.func, ast.Name):#functions
                 if n.func.id in vars:
                     vars[n.func.id]-=1
@@ -269,7 +262,6 @@ def count_func(node,vars):
                     funcs[n.func.id]=1
                 else:
                     funcs[n.func.id] += 1
-
         if isinstance(n, ast.FunctionDef):
             #funcs["def"]+=1
             if n.name not in funcs:
@@ -282,8 +274,6 @@ def count_func(node,vars):
                 funcs[n.name] = 1
             else:
                 funcs[n.name] += 1
-        elif isinstance(n, ast.Subscript):
-            funcs["[]"]+=1
 
     return funcs
 def count_skobki(text, funcs):
@@ -291,23 +281,80 @@ def count_skobki(text, funcs):
     new_word = ""
     res = 0
     temp = (3+4)*4
-    for char in text:
-        if  char not in string.ascii_letters and new_word != "":
+    kostyl = True
+    stack = []
+    for i in range(0, len(text)):
+        if text[i] == '"':
+            if kostyl:
+                kostyl = False
+                continue
+            if not kostyl:
+                kostyl = True
+                continue
+        elif not kostyl:
+            continue
+        elif text[i] == "(" and ((new_word == "" and last_word not in funcs) or new_word not in funcs) and last_word != ']' and text[i+1] != ')':
+            res+=1
+        elif text[i] not in string.ascii_letters and text[i] != '_' and new_word != "":
             last_word = new_word
             new_word = ""
-        elif char == "(" and ((new_word == "" and last_word not in funcs) or new_word not in funcs):
-            res+=1
-        else:
-            new_word+=char
+        elif text[i] != '\n':
+            new_word+=text[i]
     return res
 
+def count_skobki2(text, funcs):
+    i = 0
+    res = 0
+    while(i < len(text)):
+        curr_line = ""
+        stack = []
+        if (text[i] == "\n"):
+            i += 1
+            continue
+        a = len(text)
+        while(text[i] != '\n'):
+            curr_line += text[i]
+            i += 1
+            if i == len(text):
+                break
 
+        word_bef_bracket = ""
+        in_str = False
 
+        for j in range(0, len(curr_line)):
+            if ord(curr_line[j]) == 34:
+                if in_str:
+                    in_str = False
+                else:
+                    in_str = True
+                    continue
+            elif in_str:
+                continue
+            elif curr_line[j] != '(' and curr_line[j] != ' ':
+                word_bef_bracket += curr_line[j]
+            elif curr_line[j] == ' ':
+                word_bef_bracket = ""
+            elif curr_line[j] == '(' and word_bef_bracket == "":
+                k = 1
+                ok = True
+                stack.append(')')
+                while len(stack) != 0:
+                    if curr_line[j+k] == ')':
+                        stack.pop()
+                    elif curr_line[j+k] == '(':
+                        stack.append('(')
+                    elif curr_line[j+k] == ',' and len(stack) == 1:
+                        ok = False
+                        break
+                    k += 1
+                if(ok):
+                    res += 1
+    return res
 
 
 def parse_file(file_path):
     with open(file_path, 'r') as f:
-        text= f.read()
+        text = f.read()
         tree = ast.parse(text)
 
 
